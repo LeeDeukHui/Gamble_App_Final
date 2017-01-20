d57f558478361d6f6aad225f906f6517c35cc5d7
Jeho0216 <leejen2003@naver.com>
2017-01-20 16:26:10 +0900

    텍스트 잘림 수정(android:lines 속성 사용)

--- java/spokenlanguagelab/gamble_app_finalgit/GambleActivity.java
+++ java/spokenlanguagelab/gamble_app_finalgit/GambleActivity.java
@@ -1,7 +1,9 @@
 package spokenlanguagelab.gamble_app_finalgit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Lee Je-ho on 2017-01-20.
@@ -12,5 +14,12 @@ public class GambleActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_gamble);
+    }
+    public void prevBTN_Click(View v){
+        finish();
+    }
+    public void nextBTN_Click(View v){
+        Intent intent = new Intent(getApplicationContext(), GambleAddicationActivity.class);
+        startActivity(intent);
     }
 }
