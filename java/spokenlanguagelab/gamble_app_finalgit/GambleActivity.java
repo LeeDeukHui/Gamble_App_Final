95c0175807fa7dfa00ecdd8a03c3b83f27b9e4bf
Jeho0216 <leejen2003@naver.com>
2017-01-21 20:47:48 +0900

    로딩화면에 로딩이미지(.gif) 애니메이션(?) 적용. 적용할때 Gradle Script 안에 build.gradle 에 "dependencies { } " 안에     compile 'com.github.bumptech.glide:glide:3.7.0' <- 해당 코드 추가해야함.

--- java/spokenlanguagelab/gamble_app_finalgit/GambleActivity.java
+++ java/spokenlanguagelab/gamble_app_finalgit/GambleActivity.java
@@ -2,7 +2,6 @@ package spokenlanguagelab.gamble_app_finalgit;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
--- java/spokenlanguagelab/gamble_app_finalgit/LoadingActivity.java
+++ java/spokenlanguagelab/gamble_app_finalgit/LoadingActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 
 public class LoadingActivity extends Activity {
@@ -11,14 +15,19 @@ public class LoadingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-                setContentView(R.layout.layout_loading);
-                startLoading();
-            }
-            private void startLoading(){
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable(){
-                    public void run(){
-                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        setContentView(R.layout.layout_loading);
+
+        ImageView loading = (ImageView)findViewById(R.id.loading);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(loading);
+        Glide.with(this).load(R.drawable.loading_img).into(imageViewTarget);
+
+        startLoading();
+    }
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
