f22c3447bbd2ddb56f0999e4ccb136080030620f
Geol2 <big9401@gmail.com>
2017-01-21 17:32:27 +0900

    다시 올림ㅎ

--- java/spokenlanguagelab/gamble_app_finalgit/SelfTestActivity.java
+++ java/spokenlanguagelab/gamble_app_finalgit/SelfTestActivity.java
@@ -1,8 +1,16 @@
 package spokenlanguagelab.gamble_app_finalgit;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by big94 on 2017-01-21.
  */
 
-public class SelfTestActivity {
+public class SelfTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_selftest);
+    }
 }
